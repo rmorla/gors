a66ebf3561fc69b89640cc9fbee8fb3f0aa678c8
ricardo.morla <ricardo.morla@fe.up.pt>
2022-01-04 13:26:30 +0000

    .

--- org1-ospf/run_routing-twopeers.sh
+++ org1-ospf/run_routing-twopeers.sh
@@ -1,5 +1,4 @@
 export GORS_HOME=`pwd`
-ssh gors-target 'rm -rf ~/quagga'
-scp -r $GORS_HOME/org1-ospf/quagga gors-target:~/.
 $GORS_HOME/docker-prune.sh
+scp -r $GORS_HOME/org1-ospf/quagga gors-target:~/.
 $GORS_HOME/runremote.sh gors-target $GORS_HOME/org1-ospf/routing-twopeers.sh
