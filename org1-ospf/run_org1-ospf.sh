2299c3ced2ca471f039337f5f1d27ec4502c6f7a
ricardo.morla <ricardo.morla@fe.up.pt>
2022-01-04 13:35:59 +0000

    .

--- org1-ospf/run_org1-ospf.sh
+++ org1-ospf/run_org1-ospf.sh
@@ -1,4 +1,4 @@
 export GORS_HOME=`pwd`
 $GORS_HOME/docker-prune.sh
 scp -r $GORS_HOME/org1-ospf/quagga gors-target:~/.
-$GORS_HOME/runremote.sh gors-target $GORS_HOME/org1-ospf/routing-twopeers.sh
+$GORS_HOME/runremote.sh gors-target $GORS_HOME/org1-ospf/org1-ospf.sh
